ca192952e0e632a15c9435a0e7d9fb304e49271b
Jihoon Kim <kjh618@kaist.ac.kr>
2019-11-29 15:11:34 +0900

    Add examples

---
+++ add.asm
@@ -0,0 +1,22 @@
+// add a, b;
+subleq a, Z;
+subleq Z, b;
+subleq Z, Z;
+
+// print_int b;
+subleq b, Z;
+subleq Z, PRINT_INT;
+subleq Z, Z;
+
+// halt;
+subleq NEG_BELL, PRINT_CHAR;
+
+a: data 42;
+
+b: data 7;
+
+
+Z: data 0;
+NEG_BELL: data -7;
+PRINT_INT(-2):
+PRINT_CHAR(-1):
